9d37e0b3cb763f64bddf2bdef3b335cb79bab586
ifaronti <135629496+ifaronti@users.noreply.github.com>
2025-02-06 18:40:43 +0000

    suspence fallback

--- note-app/app/layout.tsx
+++ note-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Notification from "@/components/notifications";
+import { Suspense } from "react";
+import { Circles } from "react-loader-spinner";
 
 export const metadata: Metadata = {
   title: "Note App",
@@ -11,14 +13,27 @@ export default function RootLayout({
   children,
 }:{
   children: React.ReactNode;
-}) {
+  }) {
+  
+  const circles = (
+    <Circles
+      height="40"
+      width="40"
+      color="#355CFF"
+      ariaLabel="circles-loading"
+      wrapperClass=""
+      visible={true}
+    />
+  )
   return (
     <html lang="en">
       <body>
-        <main className="h-screen overflow-y-scroll no-scrollbar w-full 2xl:w-[1440px] mx-auto">
-          {children}
-          <Notification />
-        </main>
+        <Suspense fallback={circles}>
+          <main className="h-screen overflow-y-scroll no-scrollbar w-full 2xl:w-[1440px] mx-auto">
+            {children}
+            <Notification />
+          </main>
+        </Suspense>
       </body>
     </html>
   );
