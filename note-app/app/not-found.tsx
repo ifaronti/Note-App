f82d7f579a243c0df208d01ab031f4f16c9e247e
ifaronti <135629496+ifaronti@users.noreply.github.com>
2025-02-06 19:30:45 +0000

    not found modified

--- note-app/app/not-found.tsx
+++ note-app/app/not-found.tsx
@@ -2,5 +2,27 @@ export const dynamic = "force-dynamic";
 import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  return redirect("/login")
+  
+
+  return (
+    <div
+      className=" bg-[#E0E4EA] flex flex-col gap-5 w-full h-full items-center justify-center"
+    >
+      Page Not In Directory.
+      <button
+        onClick={redirect('/login')}
+        className="bg-[#355CFF] border-none rounded-lg w-[110px]"
+      >
+        Login
+      </button>
+
+      <button
+        onClick={redirect('/signup')}
+        className="bg-[#355CFF] border-none rounded-lg w-[110px]"
+      >
+        Register
+      </button>
+
+    </div>
+  )
 }
