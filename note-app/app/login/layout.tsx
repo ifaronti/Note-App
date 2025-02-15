ab77a48d1a72f2de8802e5a1129ab0ed553b3033
ifaronti <135629496+ifaronti@users.noreply.github.com>
2025-02-15 16:11:31 +0000

    ...

--- note-app/app/login/layout.tsx
+++ note-app/app/login/layout.tsx
@@ -1,13 +1,15 @@
-
+import { Suspense } from "react"
 import Notification from "@/components/notifications"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="w-full h-full bg-auth_page relative flex justify-center items-center">
-            {children}
-
-            <Notification/>
-
-        </section>
+        <Suspense>
+            <section className="w-full h-full bg-auth_page relative flex justify-center items-center">
+                {children}
+                
+                <Notification/>
+            
+            </section>
+        </Suspense>
     )
 }
--- note-app/app/oauth2/layout.tsx
+++ note-app/app/oauth2/layout.tsx
@@ -1,13 +1,15 @@
+import { Suspense } from "react"
 import Notification from "@/components/notifications"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        
-        <section className="bg-auth_page w-full h-full">
-            {children}
+        <Suspense>
+            <section className="bg-auth_page w-full h-full">
+                {children}
 
-            <Notification/>
+                <Notification/>
 
-        </section>
+            </section>
+        </Suspense>
     )
 }
