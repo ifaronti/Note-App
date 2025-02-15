d387460a63367c086a13646e5aea1fe677233485
ifaronti <135629496+ifaronti@users.noreply.github.com>
2025-02-15 01:27:51 +0000

    suspense or redirect debugging

--- note-app/app/dashboard/layout.tsx
+++ note-app/app/dashboard/layout.tsx
@@ -2,9 +2,10 @@ import { Suspense } from "react"
 export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
-        <section className="w-full bg-page h-full">
-            {children}
-        </section>
-
+        <Suspense fallback={<div>Loading...</div>}>
+            <section className="w-full bg-page h-full">
+                {children}
+            </section>
+        </Suspense>
     )
 }
