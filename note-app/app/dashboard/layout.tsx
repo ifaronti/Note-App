6c040075894c927eece6cc4dedcc56ccd574fc69
ifaronti <135629496+ifaronti@users.noreply.github.com>
2025-02-15 16:03:45 +0000

    ..

--- note-app/app/dashboard/layout.tsx
+++ note-app/app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
+import { Suspense } from "react"
 import Notification from "@/components/notifications"
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
-        <section className="w-full bg-page h-full">
-            {children}
-            <Notification/>
-        </section>
+        <Suspense>
+            <section className="w-full bg-page h-full">
+                {children}
+                <Notification/>
+            </section>
+        </Suspense>
     )
 }
