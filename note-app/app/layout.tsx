dda673cda139353666fbec556a66780112ff804b
ifaronti <135629496+ifaronti@users.noreply.github.com>
2025-02-15 00:38:35 +0000

    debugging

--- note-app/app/layout.tsx
+++ note-app/app/layout.tsx
@@ -17,12 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-      <Suspense fallback={<div>Loading...</div>}>
           <main className="h-screen overflow-y-scroll no-scrollbar w-full 2xl:w-[1440px] mx-auto">
               {children}
               <Notification />
           </main>
-        </Suspense>
       </body>
     </html>
   );
